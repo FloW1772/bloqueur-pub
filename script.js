892d25a0b9d9186b7232ed40742e4b11ae57cc70
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2024-01-26 10:54:34 +0100

    ajout nano adblocker et blocage avec la taille

--- script.js
+++ script.js
@@ -2,51 +2,58 @@ const TwitchAdblocker = {
 
   // Méthode pour bloquer les publicités vidéo
   blockVideoAds() {
-   // Obtenez l'élément de la balise vidéo
-   const videoElement = document.querySelector("video");
-  
-   // Obtenez la bibliothèque de filtrage de contenu
-   const adblock = new uBlockOrigin();
-  
-   // Bloquez les publicités qui apparaissent ou disparaissent au cours de la lecture de la vidéo
-   videoElement.addEventListener("visibilitychange", () => {
-    adblock.update();
-   });
-  
-   // Bloquez les publicités qui apparaissent ou disparaissent lorsque l'utilisateur fait défiler la page Web
-   document.addEventListener("scroll", () => {
-    adblock.update();
-   });
+    // Obtenez l'élément de la balise vidéo
+    const videoElement = document.querySelector("video");
+
+    // Obtenez la bibliothèque de filtrage de contenu
+    const adblock = new uBlockOrigin();
+
+    // Bloquez les publicités qui apparaissent ou disparaissent au cours de la lecture de la vidéo
+    videoElement.addEventListener("visibilitychange", () => {
+      adblock.update();
+    });
+
+    // Bloquez les publicités qui apparaissent ou disparaissent lorsque l'utilisateur fait défiler la page Web
+    document.addEventListener("scroll", () => {
+      adblock.update();
+    });
+    // Bloquez les publicités qui apparaissent ou disparaissent lorsque la fenêtre du navigateur est redimensionnée
+    window.addEventListener("resize", () => {
+      adblock.update();
+    });
   },
-  
+
   // Méthode pour bloquer les publicités contextuelles
   blockContextAds() {
-   // Obtenez la liste des éléments qui contiennent des publicités contextuelles
-   const adElements = document.querySelectorAll(".ttv_ad_container");
-  
-   // Supprimez les éléments qui contiennent des publicités contextuelles
-   for (const adElement of adElements) {
-    adElement.remove();
-   }
+    // Obtenez la liste des éléments qui contiennent des publicités contextuelles
+    const adElements = document.querySelectorAll(".ttv_ad_container");
+
+    // Supprimez les éléments qui contiennent des publicités contextuelles
+    for (const adElement of adElements) {
+      adElement.remove();
+    }
   },
-  
+
   // Méthode pour bloquer les publicités qui sont chargées dynamiquement
   blockDynamicAds() {
-   // Obtenez la bibliothèque de filtrage de contenu
-   const adblock = new uBlockOrigin();
-  
-   // Bloquez les publicités qui sont chargées de manière dynamique
-   adblock.block("script[src*='twitch.tv/embed/']");
+    // Obtenez la bibliothèque de filtrage de contenu
+    const adblock = new uBlockOrigin();
+
+    // Bloquez les publicités qui sont chargées de manière dynamique
+    adblock.block("script[src*='twitch.tv/embed/']");
+
+    // Ajoutez des règles de filtrage personnalisées
+    adblock.addRule("script[src*='twitch.tv/ad/']", "block");
   },
-  
+
   // Méthode pour initialiser l'extension
   init() {
-   // Activez les méthodes pour bloquer les publicités
-   this.blockVideoAds();
-   this.blockContextAds();
-   this.blockDynamicAds();
+    // Activez les méthodes pour bloquer les publicités
+    this.blockVideoAds();
+    this.blockContextAds();
+    this.blockDynamicAds();
   }
- };
-  
- // Initialisation de l'extension
- TwitchAdblocker.init();
+};
+
+// Initialisation de l'extension
+TwitchAdblocker.init();
